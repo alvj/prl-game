f3a0aa8ad78e121c07f6f61af034b2a5f95a1818
Álvaro <alvarojperezperez@gmail.com>
2020-05-23 02:59:38 +0200

    Añadido movimiento del personaje

---
+++ Assets/_GameAssets/_Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+    }
+}
