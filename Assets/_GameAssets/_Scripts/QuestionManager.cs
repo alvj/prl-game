373ff79d88de8656b46c3faf5146eafdabc8ef47
Álvaro <alvarojperezperez@gmail.com>
2020-05-24 01:29:09 +0200

    Starting question manager and question trigger

---
+++ Assets/_GameAssets/_Scripts/QuestionManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionManager : MonoBehaviour {
+    [SerializeField] Text questionText;
+
+    public void DisplayQuestion(Question question) {
+        questionText.text = question.GetQuestion();
+    }
+}
