447706468168c97cac8795b1c9163d23a7a0460c
Álvaro <alvarojperezperez@gmail.com>
2020-05-23 21:02:16 +0200

    Removed camera follow vertically when jumping

--- Assets/_GameAssets/_Scripts/CameraFollow.cs
+++ Assets/_GameAssets/_Scripts/CameraFollow.cs
@@ -8,8 +8,11 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] float smoothSpeed = 10f;
     [SerializeField] Vector3 offset;
 
+    private void Start() {
+        transform.position = target.position + offset;
+    }
     void LateUpdate() {
-        Vector3 finalPosition = target.position + offset;
+        Vector3 finalPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, finalPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
