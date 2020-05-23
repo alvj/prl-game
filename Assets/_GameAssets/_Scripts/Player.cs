e7c65c3d8ab9b75c2dce4a59fedeaecd71c91ccb
Álvaro <alvarojperezperez@gmail.com>
2020-05-23 14:29:52 +0200

    Changed player movement to transform

--- Assets/_GameAssets/_Scripts/Player.cs
+++ Assets/_GameAssets/_Scripts/Player.cs
@@ -6,18 +6,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
 
-    Rigidbody2D rb;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
+    private void Update()
     {
         float hInput = Input.GetAxis("Horizontal");
 
-        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+        transform.position += Vector3.right * hInput * speed * Time.deltaTime;
     }
 }
