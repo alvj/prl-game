91d95b7fcc50bb75f47748173fc57dc2594f0702
Álvaro <alvarojperezperez@gmail.com>
2020-05-24 01:43:12 +0200

    Added Answers to question manager

--- Assets/_GameAssets/_Scripts/QuestionManager.cs
+++ Assets/_GameAssets/_Scripts/QuestionManager.cs
@@ -3,8 +3,12 @@ using UnityEngine.UI;
 
 public class QuestionManager : MonoBehaviour {
     [SerializeField] Text questionText;
+    [SerializeField] Text[] answerTexts = new Text[4];
 
     public void DisplayQuestion(Question question) {
         questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerTexts.Length; i++) {
+            answerTexts[i].text = question.GetAnswers()[i];
+        }
     }
 }
