a8cadcec8122f399672c59618f4a994010056474
Álvaro <alvarojperezperez@gmail.com>
2020-05-23 22:54:32 +0200

    Added Scriptable Object for questions

---
+++ Assets/_GameAssets/Question.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Nueva Pregunta", menuName = "Question")]
+public class Question : ScriptableObject
+{
+    [TextArea(3, 15)]
+    [SerializeField] string question;
+    [TextArea(2, 10)]
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswer;
+    [TextArea(4, 30)]
+    [SerializeField] string explanation;
+}
