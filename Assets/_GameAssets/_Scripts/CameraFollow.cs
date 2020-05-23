a69efb46ace8f23d36bed9de1bb8d0b503669986
Álvaro <alvarojperezperez@gmail.com>
2020-05-23 14:29:36 +0200

    Added smooth camera movement

---
+++ Assets/_GameAssets/_Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothSpeed = 10f;
+    [SerializeField] Vector3 offset;
+
+    void LateUpdate() {
+        Vector3 finalPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, finalPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
