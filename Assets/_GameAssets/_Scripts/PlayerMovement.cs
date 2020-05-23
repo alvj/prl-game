aae154eb8475f07e13babd448efc2a66c5904c94
Álvaro <alvarojperezperez@gmail.com>
2020-05-23 19:41:43 +0200

    Changed player movement to rigidbody
    Player now uses rigidbody to move. Also added sprite flip.

---
+++ Assets/_GameAssets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 10f;
+
+    SpriteRenderer sr;
+    Rigidbody2D rb;
+
+    private void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate(){
+        float hInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+
+        sr.flipX = hInput > 0.1f ? false : hInput < -0.1f ? true : sr.flipX;
+    }
+}
