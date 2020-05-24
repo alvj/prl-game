14edc4678caaf0e403a2f22b479cb998305c7c9b
Álvaro <alvarojperezperez@gmail.com>
2020-05-24 15:28:33 +0200

    Changed sprites, changed font, added sounds to UI

--- Assets/_GameAssets/_Scripts/QuestionManager.cs
+++ Assets/_GameAssets/_Scripts/QuestionManager.cs
@@ -3,62 +3,105 @@ using UnityEngine.UI;
 using UnityEngine.Animations;
 
 public class QuestionManager : MonoBehaviour
-    {
+{
     [SerializeField] GameObject questionObject;
     [SerializeField] GameObject explanationObject;
     [SerializeField] Text questionText;
     [SerializeField] Text explanationText;
     [SerializeField] GameObject[] answerButtons = new GameObject[4];
 
+    [Space]
+
+    [Header("Audio")]
+    [SerializeField] AudioClip hoverAudio;
+    [SerializeField] AudioClip clickAudio;
+    [SerializeField] AudioClip errorAudio;
+    [SerializeField] AudioClip correctAudio;
+    AudioSource audioSource;
+
     Player player;
     Question question;
     bool guessedCorrect;
 
-    private void Awake() {
+    // COLORS
+    Color32 green = new Color32(81, 255, 120, 255);
+    Color32 red = new Color32(255, 91, 91, 255);
+    Color32 defaultButtonColor = new Color32(220, 247, 255, 255);
+
+    private void Awake()
+    {
         player = GameObject.FindObjectOfType<Player>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    public void GetQuestion(Question questionFromTrigger) {
+    public void GetQuestion(Question questionFromTrigger)
+    {
         question = questionFromTrigger;
         player.SetCanMove(false);
         DisplayQuestion();
     }
 
-    public void DisplayQuestion() {
+    public void DisplayQuestion()
+    {
         questionObject.SetActive(true);
 
         questionText.text = question.GetQuestion();
         string[] answers = question.GetAnswers();
         explanationText.text = question.GetExplanation();
 
-        for (int i = 0; i < answerButtons.Length; i++) {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
             answerButtons[i].GetComponentInChildren<Text>().text = answers[i];
         }
     }
 
-    public void CheckAnswer(int answer) {
-        if (answer == question.GetCorrectAnswer()) {
-            ChangeButtonColor(answerButtons[answer], Color.green);
+    public void CheckAnswer(int answer)
+    {
+        if (answer == question.GetCorrectAnswer())
+        {
+            // Change to green color
+            ChangeButtonColor(answerButtons[answer], green);
+            audioSource.PlayOneShot(correctAudio);
+
             guessedCorrect = true;
+
             player.GetComponent<Animator>().SetBool("correctAnswer", true);
+
             Invoke("CorrectAnswer", 1.5f);
-        } else if (answer != question.GetCorrectAnswer() && !guessedCorrect) {
-            ChangeButtonColor(answerButtons[answer], Color.red);
+        }
+        else if (answer != question.GetCorrectAnswer() && !guessedCorrect)
+        {
+            // Change to red color
+            ChangeButtonColor(answerButtons[answer], red);
+            audioSource.PlayOneShot(errorAudio);
         }
     }
 
-    void ChangeButtonColor(GameObject button, Color color) {
+    void ChangeButtonColor(GameObject button, Color32 color)
+    {
         button.GetComponent<Image>().color = color;
     }
 
-    void CorrectAnswer() {
+    void CorrectAnswer()
+    {
         player.GetComponent<Animator>().SetBool("correctAnswer", false);
         questionObject.SetActive(false);
         explanationObject.SetActive(true);
     }
 
-    public void EndQuestion() {
+    public void EndQuestion()
+    {
         explanationObject.SetActive(false);
         player.SetCanMove(true);
+    }
+
+    public void Hover()
+    {
+        audioSource.PlayOneShot(hoverAudio);
+    }
+
+    public void Click()
+    {
+        audioSource.PlayOneShot(clickAudio);
     }
 }
